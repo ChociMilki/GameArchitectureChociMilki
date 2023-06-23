15f21510cd4816d0ce954e70604295e7c4a272e8
ChociMilki <chocimilki@gmail.com>
2023-06-22 18:04:51 -0800

    changed script name and fixed console error

--- Assets/_GameArchitecture/Scripts/ServiceLocator/ServiceLocatorGameManager.cs
+++ Assets/_GameArchitecture/Scripts/ServiceLocator/ServiceLocatorGameManager.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class ServiceLocatorGameManager : MonoBehaviour
 {
     private void Awake()
     {
         //craft instance
         var audioManager = new AudioManager();
         //use service locator to register particular service from IAudioManager via AudioManager
-        ServiceLocator.ReferenceEquals<IAudioManager>(audioManager);  
+        ServiceLocator.RegisterService<IAudioManager>(audioManager);  
 
     }
 }
