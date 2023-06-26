b087353d30cb4170dba26d33724f48f67ab3642b
ChociMilki <chocimilki@gmail.com>
2023-06-26 15:20:42 -0800

    code tidy

--- Assets/_GameArchitecture/Scripts/Managers/GameManager.cs
+++ Assets/_GameArchitecture/Scripts/Managers/GameManager.cs
@@ -1,5 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 /// <summary>
 /// 
@@ -14,7 +18,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private LevelManager[] levels; 
 
     /// <summary>
-    /// keeps track of current state
+    /// tt:Holds state keys 
     /// </summary>
     public enum GameState
     {
@@ -26,7 +30,7 @@ public class GameManager : MonoBehaviour
         GameEnd
     }
     /// <summary>
-    /// 
+    /// tt: Implements singleton design pattern for the Game Manager. 
     /// </summary>
     private void Awake()
     {
@@ -34,19 +38,19 @@ public class GameManager : MonoBehaviour
         else { instance = this; }
     }
     /// <summary>
-    /// 
+    ///tt: Allows public get and private set  usage of the GameManager 
     /// </summary>
     /// <returns></returns>
     public static   GameManager GetInstance() { return instance; }
 /// <summary>
-/// get/set isinputactive 
+/// tt:Boolean to check for  input activity
 /// </summary>
 /// <returns></returns>
     public bool IsInputActive() { return isInputActive; }   
 
   
     /// <summary>
-    ///  change current game state , know the current level and the state we are changing using switchcase 
+    /// tt:Grabs knowledge of the current level and changes the current game state through SwitchCase methodology. 
     /// </summary>
     public void ChangeState(GameState state, LevelManager level)
     {
@@ -56,30 +60,63 @@ public class GameManager : MonoBehaviour
         switch (currentState)
         {
            case GameState.Briefing:
-                //Do XYZ 
+                //Initialize the  briefing state 
+                StartBriefing(); 
                 Debug.Log("Game state is briefing"); break;
+
+                //Move the game state to LS once briefing complete. 
                 case GameState.LevelStart:
-                //Do XYZ
+                //Initialize the  current level 
+                InitiateLevel(); 
                 Debug.Log("Game state is level start");
                 break;
+
             case GameState.LevelIn:
                 //Do XYZ 
                 Debug.Log("Game state is level in ");
                 break;
+
             case GameState.LevelEnd:
                 //Do XYZ 
                 Debug.Log("Game state is level end");
                 break;
+
             case GameState.GameOver:
                 // Do XYZ 
                 Debug.Log("Game state is game over");
                 break;
+
             case GameState.GameEnd:
                 // Do XYZ 
                 Debug.Log("Game state is game end");
                 break; 
         }
-        // LECTURE JUNE 20 -1:28:06
+  
     }
+    /// <summary>
+    ///tt: Disables inputActivity, moves current level to start within the GameState enum. 
+    /// </summary>
+    private void StartBriefing()
+    {
+        //set boolean iIA to false. 
+        isInputActive = false;
+        // sets the currentLevel to Level Start section of the game state. 
+        ChangeState(GameState.LevelStart, currentLevel);
+        Debug.Log("Briefing Started...");
 
+    }
+ /// <summary>
+ /// tt: Initializes and allows inputActivity as well as throwing an event within LM 
+ ///  and additionally changing the game state status to the corresponding enum key. 
+ /// </summary>
+    private void InitiateLevel()
+    {
+        // set boolean 
+        isInputActive = true;
+        // LM Event call 
+        currentLevel.StartLevel(); 
+        //   changes game state
+        ChangeState(GameState.LevelIn, currentLevel);
+        Debug.Log("Level Started!");
+    }
 }
--- Assets/_GameArchitecture/Scripts/Managers/LevelManager.cs
+++ Assets/_GameArchitecture/Scripts/Managers/LevelManager.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events; 
 /// <summary>
-///  Keeps track of levels 
+///  Keeps track of levels , utilizing events to trigger state changes 
 /// </summary>
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private bool isFinalLevel; 
+    [SerializeField] private bool isFinalLevel;
 
-
+    public UnityEvent onLevelStart;
+    public UnityEvent onLevelEnd; 
 
         public void StartLevel()
     {
+        // Invokable event 
+        onLevelStart?.Invoke(); 
         Debug.Log("Level has started!"); 
     }
+    /// <summary>
+    /// tt:  Implements game end and level end 
+    /// </summary>
     public void EndLevel()
     {
-        Debug.Log("Level has ended!");
+        // invokable event 
+        onLevelEnd?.Invoke();
+       //sets  isFL to true, as well as changing current game state to game end
+        if(isFinalLevel == true)
+        {
+            GameManager.GetInstance().ChangeState(GameManager.GameState.GameEnd, this); 
+            Debug.Log("The game has ended"); 
+        }
+        // otherwise if the above boolean is false, changes the game state to level end
+        else
+        {
+            GameManager.GetInstance().ChangeState(GameManager.GameState.LevelEnd, this);
+            Debug.Log("Level has ended!");
+        }
+    
     }
         
 
